5557119b5bc17e2536a98517192d6a27de003ac2
dracangelo <kelvinmbuguaw@gmail.com>
2024-09-19 09:49:39 +0300

    added movement

--- 2048.py
+++ 2048.py
@@ -24,3 +24,46 @@ def print_board(board):
 
 def rotate_board(board):
     return [list(row) for row in zip(*board[::-1])]
+
+def merge_left(row):
+    new_row = [num for num in row if num != 0]
+    merged_row = []
+    skip = False
+    for i in range(len(new_row)):
+        if skip:
+            skip = False
+            continue
+        if i + 1 < len(new_row) and new_row[i] == new_row[i + 1]:
+            merged_row.append(new_row[i] * 2)
+            skip = True
+        else:
+            merged_row.append(new_row[i])
+    return merged_row + [0] * (SIZE - len(merged_row))
+
+def move_left(board):
+    new_board = []
+    for row in board:
+        new_board.append(merge_left(row))
+    return new_board
+
+def move(board, direction):
+    if direction == 'UP':
+        board = rotate_board(board)
+        board = move_left(board)
+        for _ in range(3):  # Rotate back to original position
+            board = rotate_board(board)
+    elif direction == 'DOWN':
+        for _ in range(3):
+            board = rotate_board(board)
+        board = move_left(board)
+        board = rotate_board(board)
+    elif direction == 'LEFT':
+        board = move_left(board)
+    elif direction == 'RIGHT':
+        for _ in range(2):
+            board = rotate_board(board)
+        board = move_left(board)
+        for _ in range(2):
+            board = rotate_board(board)
+    return board
+
